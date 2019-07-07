1d47be88e3d6ea182238ccf98b20d30705761dc2
Kelly Zick <kllyzck@gmail.com>
2019-07-07 08:05:35 -0600

    Add error page component

--- src/components/ErrorPage/index.js
+++ src/components/ErrorPage/index.js
@@ -1,11 +1,9 @@
-import React from 'react'
+import React from 'react';
 
-const NotFound = () => {
-  return (
-    <main>
-      <h1 className='error-heading'>You seem to be confused. There is nothing here.</h1>
-    </main>
-  )
-}
+export const NotFound = () => (
+  <main>
+    <h1 className='error-heading'>You seem to be confused. There is nothing here.</h1>
+  </main>
+);
 
 export default NotFound;
--- src/components/ErrorPage/index.test.js
+++ src/components/ErrorPage/index.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { NotFound } from '.';
+import { NotFound } from './index';
 
 describe('NotFound', () => {
   let wrapper;
